831efa39dbe7a601fbf16abcbdbb1e1e75151109
Juan Navarro <navnelson7@gmail.com>
2020-10-09 18:03:01 -0600

    Finalizando formulario para registrar repuestos

--- src/components/Repuestos/FormRepuetos.js
+++ src/components/Repuestos/FormRepuetos.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Container, Row ,Form, FormControl, Col, InputGroup, Card } from 'react-bootstrap';
+import { Container, Row ,Form, FormControl, Col, InputGroup, Card, Button } from 'react-bootstrap';
 const FormRepuestos = () => {
     return (
         <Fragment>
@@ -9,7 +9,7 @@ const FormRepuestos = () => {
                     <Card>
                         <Card.Body>
                             <Row>
-                                <Col sm={12}>
+                                <Col sm={6}>
                                     <InputGroup className="mb-3">
                                         <InputGroup.Prepend>
                                             <InputGroup.Text id="basic-addon1">Codigo Repuesto</InputGroup.Text>
@@ -17,14 +17,12 @@ const FormRepuestos = () => {
                                         <FormControl 
                                             placeholder="0000000000"
                                             aria-label="codigo"
-                                            aria-describedby="codigo"
+                                            aria-describedby="basic-addon1"
                                             name="codigo"
                                         />                               
                                     </InputGroup>
                                 </Col>
-                            </Row>
-                            <Row>
-                                <Col sm={4}>
+                                <Col sm={6}>
                                     <InputGroup className="mb-3">
                                             <InputGroup.Prepend>
                                                 <InputGroup.Text id="basic-addon1">Nombre</InputGroup.Text>
@@ -32,11 +30,14 @@ const FormRepuestos = () => {
                                             <FormControl 
                                                 placeholder="Nombre del Repuesto"
                                                 aria-label="nombre"
-                                                aria-describedby="nombre"
+                                                aria-describedby="basic-addon1"
                                                 name="nombre"
                                             />
                                     </InputGroup>
                                 </Col>
+                            </Row>
+                            <Row>
+                                
                                 <Col sm={4}>
                                     <InputGroup className="mb-3">
                                         <InputGroup.Prepend>
@@ -45,7 +46,7 @@ const FormRepuestos = () => {
                                         <FormControl
                                             placeholder="00"
                                             aria-label="cantidad"
-                                            aria-describedby="cantidad"
+                                            aria-describedby="basic-addon1"
                                             name="cantidad"
                                         />
                                     </InputGroup>
@@ -53,17 +54,155 @@ const FormRepuestos = () => {
                                 <Col sm={4}>
                                     <InputGroup className="mb-3">
                                         <InputGroup.Prepend>
+                                            <InputGroup.Text>$</InputGroup.Text>
                                             <InputGroup.Text id="basic-addon1">Precio</InputGroup.Text>
                                         </InputGroup.Prepend>
                                         <FormControl
                                             placeholder="0.00"
                                             aria-label="precio"
-                                            aria-describedby="precio"
+                                            aria-describedby="basic-addon1"
                                             name="precio"
                                         />
                                     </InputGroup>
                                 </Col>
+                                <Col sm={4}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Append>
+                                            <InputGroup.Text id="basic-addon1">Medida</InputGroup.Text>
+                                        </InputGroup.Append>
+                                        <FormControl as="select" name="id_unidad_medida">
+                                            <option></option>
+                                            <option value="Galones">Galones</option>
+                                            <option value="Litros">Litros</option>
+                                        </FormControl>
+                                    </InputGroup>
+                                </Col>
                             </Row>
+                            <Row>
+                                <Col sm={4}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
+                                            <InputGroup.Text id="basic-addon1">Factura</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            placeholder="000000000"
+                                            aria-label="numero_factura"
+                                            arita-describedby="basic-addon1"
+                                            name="numero_factura"
+                                        />
+                                    </InputGroup>
+                                </Col>
+                                <Col sm={4}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Fecha Factura</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            type="date"
+                                            aria-label="fecha_factura"
+                                            aria-describedby="basic-addon1"
+                                            name="fecha_factura"
+                                        />
+                                    </InputGroup>
+                                </Col>
+                                <Col sm={4}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Fecha Ingreso</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            type="date"
+                                            aria-label="fecha_ingreso"
+                                            aria-describedby="basic-addon1"
+                                            name="fecha_ingreso"
+                                        />
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col sm={6}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Marca</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            aria-label="id_marca"
+                                            aria-describedby="basic-addon1"
+                                            name="id_marca"
+                                        />    
+                                    </InputGroup>
+                                </Col>
+                                <Col sm={6}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Proveedor</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl as="select" name="id_proveedor">
+                                            <option value="01">Impressa Repuestos</option>
+                                            <option value="02">Econo Parts</option>
+                                            <option value="03">Muller SA de CV</option>                                            
+                                        </FormControl>
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col sm={6}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Usuario</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl 
+                                            placeholder="usuario"
+                                            aria-label="id_usuario"
+                                            aria-describedby="basic-addon1"
+                                            name="id_usuario"
+                                            readOnly
+                                        />
+                                    </InputGroup>
+                                </Col>
+                                <Col sm={6}>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="basic-addon1">Estado Repuesto</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl as="select" name="id_estado_repuesto">
+                                            <option value="01">Almacenado</option>
+                                            <option value="02">Obsoleto</option>
+                                        </FormControl>
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                            <Col sm={12}>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>Comentarios</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <FormControl 
+                                        as="textarea" 
+                                        aria-label="Comentarios" 
+                                        name="comentarios"
+                                        //value={comentarios}
+                                        //onChange={onChange}
+                                    />
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={3}>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check 
+                                        type="checkbox" 
+                                        label="Activo" 
+                                        name="estado" 
+                                        //value={estado}
+                                        //onChange={handleCheck}
+                                        />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Button varian="Primera" size="lg">Guardar</Button>
                         </Card.Body>
                     </Card>
                 </Form>
