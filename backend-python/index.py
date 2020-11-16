0c3afaeb7f334b32f0500dfb29d8b6824cff9eae
Nelson-developer <nelsonher019@gmail.com>
2020-11-15 20:34:21 -0600

    Mostrando imagenes correctamente

--- backend-python/index.py
+++ backend-python/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 import os
 from PIL import Image
@@ -8,6 +8,11 @@ import typing
 app = Flask(__name__)
 app.config["CLIENT_IMAGES"] = "./images"  # files folder
 
+#CREA EL FOLDER IMAGES SI NO EXISTE
+@app.before_first_request
+def create_folder():
+    if os.path.exists(os.getcwd() + "/images") != True:
+        os.makedirs(os.getcwd() + "/images")
 
 @app.route('/upload', methods=["POST"])
 def upload_image():
@@ -22,7 +27,7 @@ def upload_image():
             image = Image.open(os.getcwd() + "/images" +
                                "/" + filename, mode="r")
             image.thumbnail(image.size)
-            image.save(filename, optimize=True, quality=30)
+            image.save(filename, optimize=True, quality=95)
         except FileNotFoundError:
             return jsonify({
                 "message": "Error, folder does not exist",
@@ -33,6 +38,11 @@ def upload_image():
             "upload": True
         })
 
+
+@app.route('/images/<string:filename>')
+def get_images(filename):
+    image_name = str(filename)
+    return send_from_directory(directory=os.path.join(app.config['CLIENT_IMAGES']), filename=image_name, as_attachment=False, )
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, threaded=True, host="0.0.0.0")
