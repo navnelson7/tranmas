f4dd98a0f32bcfe0b44dd8c15f1b77a652c32a1e
Juan Navarro <navnelson7@gmail.com>
2020-11-21 17:20:10 -0600

    Eliminando react-to-print de las dependencias

--- frontend/src/components/Empleados/FichaEmpleado.js
+++ frontend/src/components/Empleados/FichaEmpleado.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 import {Container} from 'react-bootstrap';
 const FichaEmpleado = () => {
     return (
@@ -6,6 +6,7 @@ const FichaEmpleado = () => {
             <div className="box-left">
                 <Container>
                     <h2>FICHA DE EMPLEADO TRANMAS SA. de CV.</h2>
+                    
                 </Container>
             </div>
         </Fragment>
