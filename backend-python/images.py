9ebeda309b0ecbf1b9f7b4e31576297aabefc166
Nelson-developer <nelsonher019@gmail.com>
2020-11-30 12:27:54 -0600

    Endpoint para actualizar imagenes agregado

--- backend-python/images.py
+++ backend-python/images.py
@@ -25,7 +25,6 @@ def upload_image():
     if request.method == 'POST':
         # SI EL USUARIO NO MANDA LA IMAGEN
         if 'file' not in request.files:
-            print('jjee')
             return not_found(error="Image not found")
 
         file = request.files['file']       
@@ -67,3 +66,51 @@ def upload_image():
 def get_images(filename):
     image_name = str(filename)
     return send_from_directory(directory=os.path.join(PATH_IMAGE), filename=image_name, as_attachment=False, )
+
+
+
+
+@api_images.route('/update', methods=["POST"])
+def update_image():
+    if request.method == 'POST':
+        # SI EL USUARIO NO MANDA LA IMAGEN
+        if 'file' not in request.files:
+            return not_found(error="Image not found")
+
+        file = request.files['file']       
+
+        if file.filename == '':
+            return not_found(error="Select a file")
+
+        filename = secure_filename(file.filename)
+        filename_finally = ""
+        try:
+            # Eliminamos el archivo anterior brindado por la API de HASURA
+            if os.path.exists(os.path.join(PATH_IMAGE, request.form['previousFile'])):
+                os.remove(os.path.join(PATH_IMAGE, request.form['previousFile']))
+
+            file.save(os.path.join(PATH_IMAGE, filename))
+            filename = secure_filename(file.filename)
+            image = Image.open(os.path.join(PATH_IMAGE, filename), mode="r")
+            image.thumbnail(image.size)
+
+            # Guardamos la imagen con un id unico
+            filename_finally = f'{ObjectId()}.{str(image.format).lower()}'
+            image.save(os.path.join(PATH_IMAGE, filename_finally), optimize=True, quality=95)
+
+            # Eliminamos el archivo anterior
+            if os.path.exists(os.path.join(PATH_IMAGE, filename)):
+                os.remove(os.path.join(PATH_IMAGE, filename))
+
+        except FileNotFoundError:
+            return jsonify({
+                "message": "Error, folder does not exist",
+                "upload": False,
+                "filename": ""
+            })
+        return jsonify({
+            "message": "success",
+            "upload": True,
+            "status": 200,
+            "filename": filename_finally
+        })
