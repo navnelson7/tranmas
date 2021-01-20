09c6c63e7d2ffd27bbbb4cab11a3148021e28757
Juan Navarro <navnelson7@gmail.com>
2021-01-19 18:30:49 -0600

    Creando componente para listar faltas de empleados

---
+++ frontend/src/tablas/TablaListaFaltas.js
@@ -0,0 +1,11 @@
+import src from "*.bmp";
+import React, {Fragment} from 'react';
+const TablaListaFaltas = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+}
+
+export default TablaListaFaltas;
