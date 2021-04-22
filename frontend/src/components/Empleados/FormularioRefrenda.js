e91ee41bee3406f055945bf2f8e565f2065185a3
Juan Navarro <navnelson7@gmail.com>
2021-04-21 21:33:21 -0600

    actualizando rama heranndez

--- frontend/src/components/Empleados/FormularioRefrenda.js
+++ frontend/src/components/Empleados/FormularioRefrenda.js
@@ -11,24 +11,6 @@ import {
   Button,
 } from "react-bootstrap";
 
-<<<<<<< HEAD
-import {useMutation} from "@apollo/client";
-//import {setRefrendaOne} from  '../../graphql/Mutations';
-
-const FormularioRefrenda = ({Id,Nombre,Licencia}) => {
-
-    const [showAlert, setshowAlert] = useState(false);
-    const [IconType, setIconType] = useState("");
-    const [TextAlert, setTextAlert] = useState("");
-
-   // const [addReRefrenda] = useMutation(setRefrendaOne);
-
-    const [refrenda, setRefrenda] = useState({
-        id_empleado: '',
-        fecha_emision: '',
-        fecha_refrenda: '',
-        numero_licencia_conducir:''
-=======
 const FormularioRefrenda = ({ Id, Nombre, Licencia }) => {
   const [refrenda, setRefrenda] = useState({
     id_empleado: "",
@@ -43,96 +25,9 @@ const FormularioRefrenda = ({ Id, Nombre, Licencia }) => {
       [e.target.name]: e.target.value,
       id_empleado: Id,
       numero_licencia_conducir: Licencia,
->>>>>>> 19417dc3c5004b335c08f818d76ff1fddda49c1e
     });
   };
 
-<<<<<<< HEAD
-    const {
-        id_empleado,
-        fecha_emision,
-        fecha_refrenda,
-        numero_licencia_conducir
-    } = refrenda;
-
-    const onChange = e => {
-        setRefrenda({
-            ...refrenda,
-            [e.target.name] : e.target.value,
-            id_empleado: Id,
-            numero_licencia_conducir:Licencia
-        })
-    }
-
-    const onSubmit = (e) => {
-        if(id_empleado.trim() === ""||
-        fecha_emision.trim() === "" ||
-        fecha_refrenda.trim() === "" ||
-        numero_licencia_conducir.trim() === ""
-        ){
-            setIconType("error")
-            setshowAlert("true")
-            setTextAlert("Debes llenar todos los campos");
-            return
-        }else{
-
-        }
-    }
-
-    return (
-        <Fragment>
-            <ToastComponent
-                showAlert={showAlert}
-                setShowAlert={setshowAlert}
-                iconType={IconType}
-                textAlert={TextAlert}
-            />
-            <h1>Agregar detalle de Refrenda a: {Nombre}</h1>
-            <Form>
-                <Row>
-                    <Col sm={6}>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Append>
-                                <InputGroup.Text id="basic -addon1">
-                                    Fecha de Emision
-                                </InputGroup.Text>
-                            </InputGroup.Append>
-                            <FormControl 
-                                aria-label="Nacimiento"
-                                aria-describedby="Nacimiento"
-                                type="date"
-                                name="fecha_emision"
-                                value={refrenda.fecha_emision}
-                                onChange={onChange}
-                            />
-                        </InputGroup>
-                    </Col>
-                    <Col sm={6}>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Append>
-                                <InputGroup.Text id="basic -addon1">
-                                    Fecha de Refrenda
-                                </InputGroup.Text>
-                            </InputGroup.Append>
-                            <FormControl 
-                                aria-label="Nacimiento"
-                                aria-describedby="Nacimiento"
-                                type="date"
-                                name="fecha_refrenda"
-                                value={refrenda.fecha_refrenda}
-                                onChange={onChange}
-                            />
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Button variant="danger" onClick={onSubmit}>  Agregar Refrenda <FontAwesomeIcon icon={faAddressCard}></FontAwesomeIcon></Button>
-            </Form>
-        </Fragment>
-    );
-}
-
-export default FormularioRefrenda;
-=======
   const onSubmit = (e) => {
     console.log("clicando");
   };
@@ -187,4 +82,3 @@ export default FormularioRefrenda;
 };
 
 export default FormularioRefrenda;
->>>>>>> 19417dc3c5004b335c08f818d76ff1fddda49c1e
