892d5bb779713151f987a44606d436bf6a31e2b4
Nelson-developer <nelsonher019@gmail.com>
2021-04-27 16:12:52 -0600

    Agregando cambios para colocar en produccion a React

--- backend-python/index.py
+++ backend-python/index.py
@@ -9,25 +9,31 @@ from accidentes import api_accidente
 app = Flask(__name__)
 
 app.config["CLIENT_IMAGES"] = "./images"  # files folder
-app.register_blueprint(api_images)
-app.register_blueprint(api_accidente)
+app.register_blueprint(api_images, url_prefix='/flask')
+app.register_blueprint(api_accidente, url_prefix='/flask')
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-#MIDDLEWARE QUE CREA EL FOLDER IMAGES SI NO EXISTE
+# MIDDLEWARE QUE CREA EL FOLDER IMAGES SI NO EXISTE
+
+
 @app.before_first_request
 def create_folder_images():
     if os.path.exists(os.getcwd() + "/images") != True:
         os.makedirs(os.getcwd() + "/images")
 
-#MIDDLEWARE QUE CREA EL FOLDER IMAGES SI NO EXISTE
+# MIDDLEWARE QUE CREA EL FOLDER IMAGES SI NO EXISTE
+
+
 @app.before_first_request
 def create_folder_accidentes():
     if os.path.exists(os.getcwd() + "/imagenes-accidentes") != True:
         os.makedirs(os.getcwd() + "/imagenes-accidentes")
 
+
 @app.route('/')
 def index():
     return "¡Hello!"
 
+
 if __name__ == "__main__":
-    app.run(debug=True,port=5000, threaded=True, host="0.0.0.0")
+    app.run(debug=True, port=5000, threaded=True, host="0.0.0.0")
