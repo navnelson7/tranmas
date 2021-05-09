15e3de4fbbcb35066dfcece8e4b16ea1e6fb2c21
Juan Navarro <navnelson7@gmail.com>
2021-05-09 17:49:37 -0600

    agregando formulario para registro de contrato en formato pdf

--- frontend/src/components/Empleados/FormularioContratos.js
+++ frontend/src/components/Empleados/FormularioContratos.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import {FronAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAddresCard} from "@fortawesome/free-solid-svg-icons";
+import {
+    Form,
+    InputGroup,
+    FormControl,
+    Col,
+    Row,
+    Button,
+} from 'react-bootstrap';
+import {useMutation} from "@apollo/client"
+
+import {ToastComponent} from "../Toast"; 
 const FormularioContratos = () => {
-    return (  );
+    return ( 
+        <Fragment>
+            
+        </Fragment>
+     );
 }
  
 export default FormularioContratos;
