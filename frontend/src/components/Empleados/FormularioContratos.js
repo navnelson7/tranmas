8b4cbfcf4021796745082bb85d3105bea0fb9c37
Juan Navarro <navnelson7@gmail.com>
2021-05-11 08:52:24 -0600

    agregando mutation para guardar contratos digitales

--- frontend/src/components/Empleados/FormularioContratos.js
+++ frontend/src/components/Empleados/FormularioContratos.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
-import {FronAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAddresCard} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
+
 import {
     Form,
     InputGroup,
@@ -10,15 +11,19 @@ import {
     Button,
 } from 'react-bootstrap';
 import {useMutation} from "@apollo/client"
-
+import {setContratosOne} from "../../graphql/Mutations";
 import {ToastComponent} from "../Toast"; 
-const FormularioContratos = () => {
+const FormularioContratos = ({Nombre}) => {
 
     const [showAlert, setShowAlert] = useState(false);
     const [IconType, setIconType] = useState("")
     const [TextAlert, setTextAlert] = useState("");
 
+    const [addContrato] = useMutation(setContratosOne);
 
+    const onSubmit = (e) => {
+
+    }
     return ( 
         <Fragment>
             <ToastComponent 
@@ -27,7 +32,42 @@ const FormularioContratos = () => {
                 iconType={IconType}
                 textAlert={TextAlert}
             />
-            <h1>Agregar contrato digitalizado</h1>
+            <h1>Agregar contrato digitalizado a: {Nombre}</h1>
+            <Form>
+                <Row>
+                    <Col sm={6}>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Append>
+                                <InputGroup.Text>
+                                    Fecha de Registro
+                                </InputGroup.Text>
+                            </InputGroup.Append>
+                            <FormControl 
+                                aria-label="Registro"
+                                aria-describedby="Registro"
+                                type="date"
+                                name="fecha_registro"
+                                //value={contrato.fecha_registro}
+                                //onChange={onChange}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col sm={6}>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Append>
+                                <InputGroup.Text>
+                                    Cargar Archivo
+                                </InputGroup.Text>
+                                <Form.File id="contrato_digital" label="contrato_digital" />
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Button variant="success" onClick={onSubmit}>
+                    Guardar Contrato
+                    <FontAwesomeIcon icon={faAddressCard}></FontAwesomeIcon>
+                </Button>
+            </Form>
         </Fragment>
      );
 }
