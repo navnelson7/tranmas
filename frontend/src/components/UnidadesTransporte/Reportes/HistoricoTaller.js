9f8f38d3c8bde531cc6812d0b5127b0d6baf23e6
Juan Navarro <navnelson7@gmail.com>
2021-05-16 22:19:56 -0600

    tratando de resolver un subscription

--- frontend/src/components/UnidadesTransporte/Reportes/HistoricoTaller.js
+++ frontend/src/components/UnidadesTransporte/Reportes/HistoricoTaller.js
@@ -4,11 +4,28 @@ import { Container, Table } from "react-bootstrap";
 import {useSubscription} from "@apollo/client";
 import {historicoTaller} from "../../../graphql/Suscription";
 
-import BusquedaFechas from "../Reportes/BusquedaFechas";
+//import BusquedaFechas from "../Reportes/BusquedaFechas";
+import {  Row, Form, Col, InputGroup, FormControl } from 'react-bootstrap';
 
-const HistoricoTaller = (fechaFin,fechaInicio) => {
+const HistoricoTaller = () => {
 
     const [listadoHistorico, setListadoHistorico] = useState([]);
+    const [fechaInicio, setFechaInicio] = useState({
+        fecha_inicio: ""
+    });
+
+    const {
+        fecha_inicio
+    } = fechaInicio;
+
+    const [ fechaFin, setFechaFin] = useState({
+        fecha_fin: ""
+    });
+
+    const {
+        fecha_fin
+    } = fechaFin;
+   
     const {loading, data} = useSubscription(
         historicoTaller,
         {
@@ -19,6 +36,20 @@ const HistoricoTaller = (fechaFin,fechaInicio) => {
         }
         );
 
+   
+
+    const onChange = (e) => {
+        setFechaInicio({
+            ...fechaInicio,
+            [e.target.name]: e.target.value
+        });
+        setFechaFin({
+            ...fechaFin,
+            [e.target.name]: e.target.value
+        });
+    }
+   
+
     useEffect(() =>{
         if(loading){
             return;
@@ -26,18 +57,52 @@ const HistoricoTaller = (fechaFin,fechaInicio) => {
         if(data){
             setListadoHistorico(data.registro_taller);
         }
-        console.log(listadoHistorico);
-    }, 1[data, loading]);
+    },[data, loading]);
     return ( 
         <Fragment>
             <Container>
+            
                 <div className="box-left">
-
+                <h3>Selecciona rango de fechas</h3>
+                <Form>
+                    <Row>
+                        <Col sm={6}>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="basic -addon1">
+                                        Fecha de Inicio
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                                <FormControl 
+                                    aria-label="Fecha Inicio"
+                                    aria-describedby="Fecha Inicio"
+                                    type="date"
+                                    name="fecha_inicio"
+                                    value={fecha_inicio}
+                                    onChange={onChange}
+                                />
+                            </InputGroup>
+                        </Col>
+                        <Col sm={6}>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="basic -addon1">
+                                        Fecha Fin
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                                <FormControl 
+                                    aria-label="Fecha Fin"
+                                    aria-describedby="Fecha Fin"
+                                    type="date"
+                                    name="fecha_fin"
+                                    value={fecha_fin}
+                                    onChange={onChange}
+                                />
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                </Form>
                     <h1>HISTORICO TALLER</h1>
-                    <BusquedaFechas 
-                        fechaFin={fechaFin}
-                        fechaInicio={fechaInicio}
-                    />
                     <Table striped bordered hover>
                         <thead>
                             <tr>
