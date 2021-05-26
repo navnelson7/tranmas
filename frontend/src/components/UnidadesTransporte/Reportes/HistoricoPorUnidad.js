1417a2e1f359e8405dffd0c0a104d2365d11e631
Juan Navarro <navnelson7@gmail.com>
2021-05-25 21:26:25 -0600

    agregando sstate para el historico por unidad

--- frontend/src/components/UnidadesTransporte/Reportes/HistoricoPorUnidad.js
+++ frontend/src/components/UnidadesTransporte/Reportes/HistoricoPorUnidad.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Fragment } from 'react';
 import {Container} from "react-bootstrap";
 import {Historico} from "../Reportes/Historico";
 import {useSuscription} from "@apollo/client";
 
+import {Row, Form, Col, InputGroup, FormControl} from "react-bootstrap";
+import {v4 as uuidv4} from 'uuid';
+
 const HistoricoPorUnidad = () => {
+    let total = 0;
+    const [listadoHistorico, setListadoHistorico] = useState([]);
+    const [fechaInicio, setFechaInicio] = useState({
+        fechaInicio: "",
+    });
+
+    const [fechaFin, setFechaFin] = useState({
+        fechaFin:"",
+    }); 
+
+    const onChange = (e) =>{
+        setFechaInicio({
+            ...fechaInicio,
+            [e.target.name]: e.target.value,
+        });
+        setFechaFin({
+            ...fechaFin,
+            [e.target.name]: e.target.value,
+        });
+
+    }
     return ( 
         <Fragment>
             <Container>
