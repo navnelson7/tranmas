aa2d19bf426b66f33116fffb156c4ca27317f23d
Juan Navarro <navnelson7@gmail.com>
2021-06-03 09:47:23 -0600

    mostrando selector de rango de fechas para reporte de faltas

--- frontend/src/components/Empleados/Reportes/ReporteFaltas.js
+++ frontend/src/components/Empleados/Reportes/ReporteFaltas.js
@@ -1,15 +1,56 @@
 import React from 'react';
-import { Fragment } from 'react';
-import {Container, Form, Row} from 'react-bootstrap';
+import { Fragment, useState } from 'react';
+import {Container, Form, Row, InputGroup, FormControl, Col} from 'react-bootstrap';
 const ReporteFaltas = () => {
+    const [fechaInicio, setFechaInicio] = useState({
+        fechaInicio: "",
+    });
+
+    const [fechaFin, setFechaFin] = useState({
+        fechaFin:"",
+    })
+
+    
     return ( 
         <Fragment>
             <Container>
                 <div className="box-left">
                     <h3>Seleccionar Rango de Fechas</h3>
                     <Form>
-                        <Row sm={6}>
-                            
+                    <Row>
+                        <Col sm={6}>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="basic -addon1">
+                                    Fecha de Inicio
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                                <FormControl
+                                    aria-label="Fecha Inicio"
+                                    aria-describedby="Fecha Inicio"
+                                    type="date"
+                                    name="fechaInicio"
+                                    value={fechaInicio.fechaInicio === null ? "" :fechaInicio.fechaInicio}
+                                   // onChange={onChange}
+                                />
+                                </InputGroup>
+                        </Col>
+                        <Col sm={6}>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="basic -addon1">
+                                        Fecha Fin
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                                <FormControl 
+                                    aria-label="Fecha Fin"
+                                    aria-describedby="Fecha Fin"
+                                    type="date"
+                                    value={fechaFin.fechaFin === null ? "" : fechaFin.fechaFin}
+                                    //onChange={onChange}
+                                />
+                            </InputGroup>
+                        </Col>
                         </Row>
                     </Form>
 
