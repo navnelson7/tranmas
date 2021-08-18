cff3aa69cdf53d1c55c45e2e5541fc4234a794da
Nelson-developer <nelsonher019@gmail.com>
2021-08-18 11:07:54 -0600

    Hook para hacer submit con Apollo Client

---
+++ frontend/src/hooks/useSubmitGraphQL.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export const useSubmitGraphQL = ({
+  mutationSubmit,
+  variables,
+  pushUrl = "url",
+  execute = false,
+}) => {
+  const { push } = useHistory();
+
+  //ALERTA
+  const [TextAlert, setTextAlert] = useState("");
+  const [showAlert, setshowAlert] = useState(false);
+  const [IconType, setIconType] = useState("");
+  const [Loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (execute) {
+      const executeSubmit = async () => {
+        try {
+          await mutationSubmit({ variables });
+          setLoading(false);
+          setIconType("success");
+          setshowAlert(true);
+          setTextAlert("Registrado correctamente");
+          push(pushUrl);
+        } catch (error) {
+          setLoading(false);
+          setTextAlert(error.message);
+          setIconType("error");
+          setshowAlert(true);
+        }
+      };
+      executeSubmit();
+    }
+  }, [execute]);
+
+  return {
+    TextAlert,
+    showAlert,
+    IconType,
+    Loading,
+    setshowAlert,
+  };
+};
